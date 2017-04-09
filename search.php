f6f1eae3303ce064ecdbea30d8c6f2fb7bfd12e8
Alex-Tsvetanov <alex_tsvetanov_2002@abv.bg>
2017-04-09 20:19:04 +0300

    fix search resulting

--- search.php
+++ search.php
@@ -21,19 +21,19 @@
 	$users_html = "";
 	while ($users->num_rows > 0 and $row = $users->fetch_assoc ())
 	{
-		$users_html = $users_html . "<div class=\"col-md-4\"><h2 id=\"{$row["ID"]}\">{$row["Name"]}</h2></div>";
+		$users_html = $users_html . "<div class=\"course-item col-md-4\"><h2 id=\"{$row["ID"]}\">{$row["Name"]}</h2></div>";
 	}
 
 	$contests_html = "";
 	while ($contests->num_rows > 0 and $row = $contests->fetch_assoc ())
 	{
-		$contests_html = $contests_html . "<div class=\"col-md-4\"><h2 id=\"{$row["ID"]}\">{$row ["Name"]}</h2><span data-toggle=\"tooltip\" data-placement=\"left\" title=\"Is this constest for a certificate?\" data-original-title=\"Is this constest for a certificate?\"><i class=\"fa fa-trophy\" aria-hidden=\"true\"></i> " . (($row["Certify"] == 0)?"No":"Yes") . "<br><a href=\"{$row["Link"]}\" role=\"button\" class=\"btn btn-raised btn-success\">Compete »</a><a href=\"{$row["Link"]}/results\" role=\"button\" class=\"btn btn-default active\">View results »</a></span></div>";
+		$contests_html = $contests_html . "<div class=\"course-item col-md-4\"><h2 id=\"{$row["ID"]}\">{$row ["Name"]}</h2><span data-toggle=\"tooltip\" data-placement=\"left\" title=\"Is this constest for a certificate?\" data-original-title=\"Is this constest for a certificate?\"><i class=\"fa fa-trophy\" aria-hidden=\"true\"></i> " . (($row["Certify"] == 0)?"No":"Yes") . "<br><a href=\"{$row["Link"]}\" role=\"button\" class=\"btn btn-raised btn-success\">Compete »</a><a href=\"{$row["Link"]}/results\" role=\"button\" class=\"btn btn-default active\">View results »</a></span></div>";
 	}
 
 	$quizes_html = "";
 	while ($quizes->num_rows > 0 and $row = $quizes->fetch_assoc ())
 	{
-		$quizes_html = $quizes_html . "<div class=\"col-md-4\"><h2 id=\"{$row["ID"]}\">{$row ["Name"]}</h2><br><a href=\"{$row["Link"]}\" role=\"button\" class=\"btn btn-raised btn-success\">Compete »</a><a href=\"{$row["Link"]}/results\" role=\"button\" class=\"btn btn-default active\">View results »</a></span></div>";
+		$quizes_html = $quizes_html . "<div class=\"course-item col-md-4\"><h2 id=\"{$row["ID"]}\">{$row ["Name"]}</h2><br><a href=\"{$row["Link"]}\" role=\"button\" class=\"btn btn-raised btn-success\">Compete »</a><a href=\"{$row["Link"]}/results\" role=\"button\" class=\"btn btn-default active\">View results »</a></span></div>";
 	}
 
 	if ($contests_html == "")
